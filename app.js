ffcd5ab0796714290c14d2e7dc05cae391cd7102
Didier Gayrard <didier.gayrard@gmail.com>
2021-03-05 19:16:26 +0100

    [BRING UP] first hello world page

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+console.log("hello world les jeunes");
+//function foo() {
+//   if (true) {
+//     console.log("Hello World)");
+//   }
+// }
+
+/**
+ * ! Aie caramba
+ * ? est ce que ?
+ * TODO
+ * @param
+ */
